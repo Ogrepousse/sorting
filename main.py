e1580ed6cb4d242645f7d2c17f26a420d61031d9
Ogrepousse <etienne_99@hotmail.com>
2014-07-31 18:26:09 +0200

    ajout note

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 with open('../files/test.filtered', 'rb') as fd:
 	ret = -1
 	while ret == -1:
-		print('hello')
 		ret = fd.readline().find('EOH')
 
 	#debut des stream
-#	a = np.array([], dtype = 'int16')
-	a = np.fromstring(fd.read(254 * 2), dtype = np.int16)
-	for i in np.arange(10):
-		b = np.fromstring(fd.read(254 * 2), dtype = np.int16)
-		a = np.vstack((a, b))
-	print('lol')
-	print(a)
+	a = np.fromstring(fd.read(252 * 2), dtype = np.int16)[:, np.newaxis]
+	print(a.shape)
+	b = np.zeros((252, 1))
+	print('B', b.shape)
+	x = 1
+#	for i in np.arange(120):
+	while b.shape[0] == 252:
+	#	print('bbb', b.shape)
+		b = np.fromstring(fd.read(252 * 2), dtype = np.int16)[:, np.newaxis]
+		a = np.append(a, b, axis = 1)
+		x += 1
+
+b = a[0]
+print('x =', x)
+c = np.arange(x);
+print(b)
+print(c)
+plt.plot(c, b)
+plt.show()
+print('lol')
