30125048304ee2b4e00251115579e0b00df47cf8
Ogrepousse <etienne_99@hotmail.com>
2014-08-12 17:48:53 +0200

    maj mardi soir

--- libpy/dot_prod/scal_prod.py
+++ libpy/dot_prod/scal_prod.py
@@ -6,42 +6,60 @@ import scipy.io
 
 def get_temp():
 	"""recupere les templates sur un fichier matlab"""
+
 	all_temp = scipy.io.loadmat('../files/ALL.templates1')
 	temp = all_temp['templates'].astype(np.float64)
 	return (temp)
 
+
 def select_ti(ti, blc, k, a):
 	"""recupere les temps de spike ds le block en excluant les spike extreme n'ayant pas assez de points autout pour les produit scalaire"""
+
 	l = ti[np.where(ti <= blc[k])[0]]
 	if k > 0:
 		l = l[np.where(win > blc[k - 1])[0]]
 	l = l[np.where((l > 64) & (l + 65 < a.shape[1]))]
 	return (l)
 
+
 def normalize_temp(temp):
 	"""normlise les templates et renvoi un tableau contenant la norme de chaque template"""
+
 	norme = np.empty(temp.shape[2])
 	for i in range(temp.shape[2]):
 		norme[i] = np.linalg.norm(temp[:, :, i])
 		temp[:, :, i] = temp[:, :, i] / norme[i]
 	return (norme)
 
-def browse_bloc(a, blc, ti):
-	"""parcour les blocks pour appliquer le fitting"""
-	temp = get_temp()
-	norme = normalize_temp(temp)
-	for k in range(blc.shape[0]):
-		l = select_ti(ti, blc, k, a)
-		bij = get_bij(a, l, temp)
 
 def get_bij(a, l, temp):
 	"""calcul la matrice bij"""
+
 	bij = np.empty((l.shape[0], temp.shape[2]))
 	for i in range(bij.shape[0]):
 		si = a[:, l[i] - 64 : l[i] + 65]
 		for j in range(bij.shape[1]):
 			bij[i, j] = np.sum(np.dot(si, temp[:, :, j].T))
 	return (bij)
+
+
+def part_aij(bij, norme, a):
+	"""check max bij puis calcul aij verification contrainte aij"""
+
+
+
+
+def browse_bloc(a, blc, ti):
+	"""parcour les blocks pour appliquer le fitting"""
+
+	temp = get_temp()
+	norme = normalize_temp(temp)
+	for k in range(blc.shape[0]):
+		l = select_ti(ti, blc, k, a)
+		bij = get_bij(a, l, temp)
+
+
+
 
 #reecrite en plus aere
 def do_stuff(a, blc, ti):
