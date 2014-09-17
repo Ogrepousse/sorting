ee8a81814b623797a6f067fc9ed461a286b0c50c
Ogrepousse <etienne_99@hotmail.com>
2014-09-17 14:46:05 +0200

    main take arg

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
 import libpy
 from libpy import get_data
@@ -13,8 +14,13 @@ from libpy.dot_prod import scal_prod
 from libpy import environment
 from libpy.environment import class_env
 
+if __name__ != "__main__" or len(sys.argv) != 2:
+	print(__name__)
+	print(len(sys.argv))
+	exit(-1)
+
 env = class_env.t_env()
-sample = 5000
+sample = int(sys.argv[1])
 x = 0
 sig = get.get_stream(x = x, y = sample)
 a = np.reshape(sig, (-1, env.nb_elec)).T
@@ -37,9 +43,9 @@ b = a.copy()
 env.setup_env(a, ti, div)
 scal_prod.browse_block(env, a, blc, ti, div)
 
-for i in range(90, 91):
-	print(i)
-	my_plot.trace(a, b, y = sample - x, nb = i)
-	plt.show()
+#for i in range(90, 91):
+#	print(i)
+#	my_plot.trace(a, b, y = sample - x, nb = i)
+#	plt.show()
 
 print('fini')
