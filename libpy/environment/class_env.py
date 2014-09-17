1386b35448fa0858d0761c6a68d8447ba8ad1bef
Ogrepousse <etienne_99@hotmail.com>
2014-09-17 13:36:12 +0200

    docstring class_env

--- libpy/environment/class_env.py
+++ libpy/environment/class_env.py
@@ -12,33 +12,49 @@ class t_env(object):
 		print('environnement cree')
 
 	def set_temp(self):
+		"""get the templates and make a copy for normalization"""
+
 		self.temp = env_fct.get_temp()
 		self.temp2 = self.temp.copy()
 		self.temp_size = self.temp.shape[1] #template width
 		self.nb_temp = self.temp.shape[2] #number of templates
 
 	def set_comp(self):
+		"""get the template's second component"""
+
 		self.comp = snd_comp.get_comp(self.temp)
 		self.comp2 = self.comp.copy()
 
 	def set_norme(self):
+		"""normalize the templates and the second component and stocks the norme"""
+
 		self.norme = env_fct.normalize_temp(self.temp)
 		self.norme2 = env_fct.normalize_temp(self.comp)
 
 	def set_lim(self):
+		"""get limit for templates"""
+
 		self.amp_lim = env_fct.get_amp_lim()
 
 	def set_overlap(self):
+		"""get the overlap matrix"""
+
 		self.overlap = env_fct.get_overlap(self)
 
 	def set_time(self, a, ti, div):
+		"""get all the spike time for all the blocks"""
+
 		self.al, self.size = gab.get_all_time(self, a, ti, div)
 
 	def set_bij(self, a, div):
+		"""calculate all bij and beta_ij for each blocks"""
+
 		self.big_bij = gab.get_all_bij(self, a, div, self.al, self.temp, self.size)
 		self.big_beta = gab.get_all_bij(self, a, div, self.al, self.comp, self.size)
 
 	def setup_env(self, a, ti, div):
+		"""use to load all the data needed for the fitting"""
+
 		self.set_temp()
 		self.set_comp()
 		self.set_norme()
