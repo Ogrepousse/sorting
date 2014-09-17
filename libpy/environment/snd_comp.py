24eb49f3af4a004eedf2702669f1812d2a683978
Ogrepousse <etienne_99@hotmail.com>
2014-09-17 11:32:44 +0200

    del hardcode var in snd_comp.py

--- libpy/environment/snd_comp.py
+++ libpy/environment/snd_comp.py
@@ -7,7 +7,7 @@ def get_comp(temp):
 	for i in range(temp.shape[2]):
 		t = temp[:, :, i]
 		t1 = t[:, 1:]
-		t2 = t[:, :129 - 1]
-		comp[:, :129 - 1, i] = (t1 - t2).copy()
-		comp[:, 128, i] = 0
+		t2 = t[:, :temp.shape[1] - 1]
+		comp[:, :temp.shape[1] - 1, i] = (t1 - t2).copy()
+		comp[:, temp.shape[1] - 1, i] = 0
 	return (comp)
