7dda294ed8d4bae17d6c7208c47a7529ca8889e3
Ogrepousse <etienne_99@hotmail.com>
2014-10-29 10:35:34 +0100

    clean scal_prod.py

--- libpy/dot_prod/scal_prod.py
+++ libpy/dot_prod/scal_prod.py
@@ -44,13 +44,13 @@ def get_max(bij, exploration, bij_bool):
 	return (c)
 
 
-def substract_signal(a, l, aij, temp, c, alpha, comp2, limit, b):
+def substract_signal(a, l, aij, temp, c, alpha, comp2, limit):
 	"""substract the template to the signal"""
 	alpha = 0
 	a[:, l[c[0]] - 64 : l[c[0]] + 65] -= aij * temp[:, :, c[1]] + alpha * comp2[:, :, c[1]]
 
 
-def part_aij(t_env, bij, a, exploration, bij_bool, l, beta_ij, div, k, b, b_save, b_prec):
+def part_aij(t_env, bij, a, exploration, bij_bool, l, beta_ij, div, k, b_save):
 	"""get i and for max value of bij, then check if aij value is correct"""
 
 	c = get_max(bij, exploration, bij_bool)
@@ -62,12 +62,10 @@ def part_aij(t_env, bij, a, exploration, bij_bool, l, beta_ij, div, k, b, b_save
 	win2 = t_env.win_mega
 	if aij > limit[0] and aij < limit[1]:
 		if (l[c[0]] < div[k, 1] - win) and (l[c[0]] > div[k, 0] + win) and (l[c[0]] < div[-1, 1] - win2) and (l[c[0]] > div[0, 0] + win2):
-		#	aij = bij[c]
 			t_env.fdout.write(str(aij) + ' ' + str(c[1]) + ' ' + str(l[c[0]] + t_env.index) + '\n')
-			substract_signal(a, l, aij, t_env.temp, c, alpha, t_env.comp2, limit, b)
-#			maj_scalar(t_env, c, bij, beta_ij, l, aij, alpha)
-	#		maj_scalar(t_env, c, b_save, beta_ij, l, aij, alpha)
+			substract_signal(a, l, aij, t_env.temp, c, alpha, t_env.comp2, limit)
 		maj_scalar(t_env, c, bij, beta_ij, l, aij, alpha)
+#		maj_scalar(t_env, c, b_save, beta_ij, l, aij, alpha)
 		return (1)
 	else:
 		exploration[c[0]] += 1
@@ -131,29 +129,18 @@ def get_bij(a, l, temp):
 
 def browse_block(t_env, a, blc, ti, div):
 	"""browse all block in order to apply the fitting"""
-	### a supprimer ###
-	b = a.copy()
-	###################
 
 	print('parcours', blc.shape[0])
 	#parcours des blocs
-	b_save = 0
-#	print('hehe', t_env.al)
+	b_save = 0 #utilise pour le debug
 	for k in range(blc.shape[0]):
 		print('entre block', k)
-	#	t_env.fdout.write("bloque " + str(k) + "\n")
 		l = small_time(t_env.al, k, t_env.size)
-	#	print('haha', l)
 		exploration = np.zeros(l.shape[0])
 		bij = small_bij(t_env.big_bij, k, t_env.size)
-		
 		b_save = bij.copy()
 		beta_ij = small_bij(t_env.big_beta, k, t_env.size)
 		bij_bool = np.zeros(bij.shape, dtype = bool)
 		bol = 0
-		bij2 = bij
 		while is_explored(exploration, bij_bool):
-	#		if bol:
-	#			bij = get_bij(a, l, t_env.temp)
-			b_prec = bij2.copy()
-			bol = part_aij(t_env, bij, a, exploration, bij_bool, l, beta_ij, div, k, b, b_save, b_prec)
+			bol = part_aij(t_env, bij, a, exploration, bij_bool, l, beta_ij, div, k, b_save)
