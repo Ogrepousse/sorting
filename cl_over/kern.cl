0fe841e214c1a01aaa6b20a74e111752d712c247
Ogrepousse <etienne_99@hotmail.com>
2014-11-28 13:59:08 +0100

    add comment

--- cl_over/kern.cl
+++ cl_over/kern.cl
@@ -1,18 +1,44 @@
 
-
-__kernel void	test(const int x, const int y, const int z, __global float *a, __global float *res)
+//version1
+__kernel void	test_v1(const int X, const int Y, const int Z, __global float *a, __global float *res)
 {
 	uint	i = get_global_id(0);
 	uint	j = get_global_id(1);
-	int		k, l, m;
+	int		x, y, z;
 	float	tmp;
 
 	tmp = 0;
-	for (k = 0; k < x * y; k ++)
+	for (z = 0; z < X * Y; z++)
 	{
-		l = k / y;
-		m = k - l * y;
-		tmp += a[l * y * z + m * z + i] * a[l * y * z + m * z + j];
+		//decompostion des coordonnees
+		x = z / Y;
+		y = z - x * Y;
+		tmp += a[x * Y * Z + y * Z + i] * a[x * Y * Z + y * Z + j];
 	}
-	res[i * z + j] = tmp;
+	res[i * Z + j] = tmp;
+}
+
+//version2 en cours de dev
+__kernel void	test_v2(const int X, const int Y, const int Z, __global float *a, __global float *res, __local float *t1, __local float *t2)
+{
+	uint	i = get_global_id(0);
+	uint	j = get_global_id(1);
+	int		x, y, z;
+	float	tmp;
+
+	tmp = 0;
+	for (z = 0; z < X * Y; z++)
+	{
+		x = z / Y;
+		y = z - x * Y;
+		tmp += a[x * Y * Z + y * Z + i] * a[x * Y * Z + y * Z + j];
+	}
+	res[i * Z + j] = tmp;
+}
+
+__kernel void	lol(__global float* a, __local float* l)
+{
+	uint i = get_local_id(0);
+
+	l[i] = a[i];
 }
--- cl_over/main.py
+++ cl_over/main.py
@@ -3,6 +3,7 @@ import pyopencl as cl
 import pyopencl.array
 import sys
 
+#initialisation pyopencl
 plt = cl.get_platforms()
 dev = plt[0].get_devices(device_type = cl.device_type.CPU)
 ctx = cl.Context(devices = dev)
@@ -13,35 +14,56 @@ fd = open('kern.cl', 'r')
 kern = "".join(fd.readlines())
 prog = cl.Program(ctx, kern).build()
 
-x = 252
-y = 129
-z = 100
+#choix de la taille des templates
+if len(sys.argv) == 4:
+	x = int(sys.argv[1])
+	y = int(sys.argv[2])
+	z = int(sys.argv[3])
+else:
+	x = 252
+	y = 129
+	z = 382
+print(x, y, z)
 
+#creation templates et d un array pour les resultat
 a = np.arange(x * y * z, dtype = np.float32)
 A = a.reshape(x, y, z)
 res = np.empty(z * z, dtype = np.float32)
 
+#creation des buffer
 a_buf = cl.Buffer(ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf = a)
 r_buf = cl.Buffer(ctx, mf.WRITE_ONLY, res.nbytes)
+#creation des buffers dans la memoire locale
+t1 = cl.LocalMemory(A[:, :, 0].nbytes)
+t2 = cl.LocalMemory(A[:, :, 0].nbytes)
 
+#execute la version 1 du kernel et extraction des resultats
 def f1():
-	prog.test(queue, (z, z), None, np.int32(x), np.int32(y), np.int32(z), a_buf, r_buf)
+	prog.test_v1(queue, (z, z), None, np.int32(x), np.int32(y), np.int32(z), a_buf, r_buf)
 	cl.enqueue_copy(queue, res, r_buf)
-	r = res.reshape(z, z)
+	r = res.reshape(z, z).copy()
 	return (r)
 
+#idem pour v2
+def f2():
+	prog.test_v2(queue, (z, z), (z, z), np.int32(x), np.int32(y), np.int32(z), a_buf, r_buf, t1, t2)
+	cl.enqueue_copy(queue, res, r_buf)
+	r = res.reshape(z, z).copy()
+	return (r)
 
+#version python de base pour les tests
 def omeg(temp, temp2):
 #	n = temp.shape[2]
 	n = z
 	om = np.empty((n, n))
 	for j in range(0, n):
+	#	print(j)
 		t1 = temp2[:, :, j].copy()
 		for i in range(0, n):
 			t2 = temp[:, :, i].copy()
 			om[j, i] = np.sum(t1 * t2)
 	return (om)
 
-r = f1()
+#r = f1()
 print('hello')
 #om = omeg(A, A)
